4d8b8196b5ffc57bedbcd6b4743cd4d22406f402
lecto17 <khwoo_rpsit@naver.com>
2025-11-27 16:01:38 +0900

    chore: 프로젝트 배포

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+const base =
+  process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-1/' : '';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  // @ts-expect-error - vitest config
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    css: true,
-  },
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true
+  }
 });
